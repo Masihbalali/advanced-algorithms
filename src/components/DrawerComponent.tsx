6bb34834d484cdb2eeed10d703f3df8c1b167058
Masihbalali <Masihbalali@outlook.com>
2024-10-27 23:45:35 +0330

    fix: sidebar arrows.

--- src/components/DrawerComponent.tsx
+++ src/components/DrawerComponent.tsx
@@ -106,10 +106,16 @@ export function DrawerComponent() {
                         {hasChildren && (
                             isOpen ? (
                                 // <ChevronDownIcon className="h-5 w-5" />
-                                <span>false</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+                                </svg>
+
+
                             ) : (
                                 // <ChevronRightIcon className="h-5 w-5" />
-                                <span>false</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                                </svg>
                             )
                         )}
                     </ListItem>
